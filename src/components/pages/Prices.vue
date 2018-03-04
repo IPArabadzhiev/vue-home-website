0bf1b5a6057608d0496c86ff2ec0f9061fe4be5c
Ivan Arabadzhiev <vank091@gmail.com>
2018-03-04 23:22:44 +0200

    some assets

---
+++ src/components/pages/Prices.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+        msg: 'Home | Prices'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
